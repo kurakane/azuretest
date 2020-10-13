ae43f6ee862340ac0a5d27f91d25510bae28245d
kurakane <kurakane.takashi@outlook.com>
2020-10-13 15:32:36 +0900

    スタートアップスクリプトを登録

---
+++ startup/startup.sh
@@ -0,0 +1,19 @@
+wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+
+# sudo rm /var/lib/dpkg/lock
+sudo dpkg --configure -a
+
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get -f install
+
+sudo apt-get install blobfuse
+
+# sudo mkdir /mnt/data-in
+# sudo mkdir /mnt/blobfusetmp-in -p
+# sudo blobfuse /mnt/data-in --tmp-path=/mnt/blobfusetmp-in --config-file=/mnt/batch/tasks/startup/wd/fuse_conn_in.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -o allow_other
+
+# sudo mkdir /mnt/data-out
+# sudo mkdir /mnt/blobfusetmp-out -p
+# sudo blobfuse /mnt/data-out --tmp-path=/mnt/blobfusetmp-out --config-file=/mnt/batch/tasks/startup/wd/fuse_conn_out.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -o allow_other
