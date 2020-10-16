11023bf1732d9a6a1acad94fdf1542f859db8aab
kurakane <kurakane.takashi@outlook.com>
2020-10-16 15:34:58 +0900

    スタートアップスクリプト修正

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,19 +1,32 @@
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 
 # これを実行しないとファイルがロックされていて落ちる. どれが悪さをしているかは不明.
-sudo rm /var/lib/dpkg/lock
-sudo rm /var/lib/dpkg/lock-frontend
+# sudo rm /var/lib/dpkg/lock
+# sudo rm /var/lib/dpkg/lock-frontend
 # これを実行しないと blobfuseがインストールできないので追加.
-sudo dpkg --configure -a
+# sudo dpkg --configure -a
 
 sudo dpkg -i packages-microsoft-prod.deb
+
+# 先にゴミを削除しないと落ちるパターンあり.
+# sudo rm -rf /var/lib/apt/lists/*
+# sudo apt-get autoclean
+# sudo apt-get clean
 
 sudo apt-get update
 
 # これを実行しないと blobfuseがインストールできないので追加.
 sudo apt-get -f install
 
+#sudo add-apt-repository main
+#sudo add-apt-repository universe
+#sudo add-apt-repository restricted
+
 sudo apt-get install blobfuse
+
+# ロックしているプロセスを見る
+# ps aux | grep [a]pt
 
 sudo mkdir /mnt/data-in
 sudo mkdir /mnt/blobfusetmp-in -p
